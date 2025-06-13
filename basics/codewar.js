fac5ed92228b2595c5e2e2dd8c12e64dd6a6ef1f
dungasamsungo <sampsonngobi@gmail.com>
2025-06-12 21:58:05 -0300

    code challenge done

--- basics/codewar.js
+++ basics/codewar.js
@@ -229,4 +229,22 @@ function oddOrEven(array){
 
 }
 
-console.log(oddOrEven([]))
+//console.log(oddOrEven([]))
+
+
+/* Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+ For example, for [1, 2, 2] it should return 9 because */
+
+ function squareSum(numbers){
+
+  let count = 0;  
+  for(let i = 0; i < numbers.length; i++){
+    const squareRoot = numbers[i] ** 2;
+    count += squareRoot
+  }
+
+    return count
+}
+
+console.log(squareSum([1,2,2]));
